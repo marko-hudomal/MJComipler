2f4a1fab83ef2bc259790db1b4c51838c1924c8a
Marko Hudomal <markohudomal@yahoo.com>
2019-01-05 20:36:43 +0100

    lexer

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,61 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+	public static final int error = -1;
+	public static final int EOF=0;
+	
+	public static final int PROGRAM=1;
+	public static final int BREAK=2;
+	public static final int CLASS=3;
+	public static final int INTERFACE=4;	
+	public static final int ENUM=5;
+	public static final int ELSE=6;
+	public static final int CONST=7;
+	public static final int IF=8;
+	public static final int NEW=9;
+	public static final int PRINT=10;
+	
+	public static final int READ=36;
+	public static final int RETURN=37;
+	public static final int VOID=38;
+	public static final int FOR=39;
+	public static final int EXTENDS=40;
+	public static final int CONTINUE=41;
+	
+	
+	//operatori
+	public static final int PLUS = 11;
+	public static final int MINUS=12;
+	public static final int MUL=13;
+	public static final int DIV=14;
+	public static final int PERC=15;
+	
+	public static final int EQUAL_EQUAL=16;
+	public static final int NOT_EQUAL=17;
+	public static final int GREATER=18;
+	public static final int GREATER_EQUAL=19;
+	public static final int LESS=20;
+	public static final int LESS_EQUAL=21;
+	public static final int AND=22;
+	public static final int OR=23;
+	
+	public static final int EQUAL=24;
+	public static final int PLUS_PLUS=25;
+	public static final int MINUS_MINUS=26;
+	
+	public static final int SEMI=27;
+	public static final int COMMA=28;
+	public static final int DOT=29;
+	
+	public static final int LPAREN=30;
+	public static final int RPAREN=31;
+	public static final int LBRACKET=32;
+	public static final int RBRACKET=33;
+	public static final int LBRACE=34;
+	public static final int RBRACE =35;
+	
+	public static final int CHAR=42;
+	public static final int IDENT=43;
+	public static final int BOOLEAN=44;
+	public static final int NUMBER=45;
+}
